24425873ff30f0bd7ce0a9b03e531797219a88e8
ALFF Oliver <O.ALFF@ctisystems.com>
2022-03-14 13:52:08 +0100

    Changed README.md

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"sma-speedwire adapter settings": {
+		"en": "Adapter settings for sma-speedwire",
+		"de": "Adaptereinstellungen für sma-speedwire",
+		"ru": "Настройки адаптера для sma-speedwire",
+		"pt": "Configurações do adaptador para sma-speedwire",
+		"nl": "Adapterinstellingen voor sma-speedwire",
+		"fr": "Paramètres d'adaptateur pour sma-speedwire",
+		"it": "Impostazioni dell'adattatore per sma-speedwire",
+		"es": "Ajustes del adaptador para sma-speedwire",
+		"pl": "Ustawienia adaptera dla sma-speedwire",
+		"zh-cn": "sma-speedwire的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
